94572fb52f00bde3c72c0f14d5c4068b79b10d81
therealsamas <therealsamas@gmail.com>
2023-11-17 01:01:41 +0530

    1stapiready

--- backend/api_controllers/getroomdat_controller.js
+++ backend/api_controllers/getroomdat_controller.js
@@ -5,18 +5,101 @@ const ObjectId = mongoose.Types.ObjectId;
 
 
 module.exports.getroomdat = async function(req,res){
-	console.log('api hit');
-	// let roomname = req.query.name;
-	// let roomdate = req.query.date;
-	const room = await Room.find();
-	let resobj = {};
-	// console.log(room[0]);
-	for(let key in room[0].occupancy){
-		console.log(key);
-	}
-	return res.status(200).json({
-		data:{
-			room
+	try{
+		console.log('api hit');
+		// let roomname = req.query.name;
+		// let roomdate = req.query.date;
+		const rooms = await Room.find();
+		const date = new Date();
+		let day = date.getDate();
+		let month = date.getMonth() + 1;
+		let year = date.getFullYear();
+		let currentDate = `${day}-${month}-${year}`;
+
+		let resobj = {};
+
+		for(let i=0; i<rooms.length; i++){
+			let missing = 0;
+			for(let date in rooms[i].occupancy){
+				missing++;
+				for(let key in rooms[i].occupancy){
+					let keyday = key[0];
+					if(key[1] != '-'){
+						keyday+=key[1];
+					}
+					if(keyday < day){
+						console.log('deleting an old date');
+						missing--;
+						delete rooms[i].occupancy[key];
+					}
+					// console.log(key);
+				}
+			}
+			let curdate = date;
+			while(4-missing>0){
+				let nextdate = new Date(curdate);
+				nextdate.setDate(curdate.getDate() + missing);
+				let day = nextdate.getDate();
+				let month = nextdate.getMonth() + 1;
+				let year = nextdate.getFullYear();
+				let newnextdate = `${day}-${month}-${year}`;
+				rooms[i].occupancy[newnextdate] = 
+				{
+			      "9-10": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "10-11": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "11-12": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "12-1": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "1-2": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "2-3": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "3-4": {
+			        "status": "free",
+			        "occupant": "nil"
+			      },
+			      "4-5": {
+			        "status": "free",
+			        "occupant": "nil"
+			      }
+	    		};
+	    		missing++;
+			}
 		}
-	});
+		for(let date in rooms[0].occupancy){
+			resobj[date] = {};
+		}
+		for(let i=0; i<rooms.length; i++){
+			for(let date in rooms[i].occupancy){
+				let name = rooms[i].name;
+				resobj[date][name]= rooms[i].occupancy[date];
+			}	
+		}
+		console.log(resobj["17-11-2023"]);
+		for(let i=0; i<rooms.length; i++){
+			await Room.findByIdAndUpdate(rooms[i].id, rooms[i]);
+
+		}
+		return res.status(200).json({
+				resobj
+		});
+	}catch(error){
+		console.log(error);
+	}
+	
 }
--- backend/index.js
+++ backend/index.js
@@ -39,5 +39,19 @@ app.listen(port, function(err){
 	if(err){
 		console.error(err);
 	}
+	const date = new Date();
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	let year = date.getFullYear();
+	// console.log(getDate("23-11-2023"));
+	// This arrangement can be altered based on how we want the date's format to appear.
+	// let currentDate = `${day}-${month}-${year}`;
+	console.log(date);
+	let tomorrow = new Date(date);
+	tomorrow.setDate(date.getDate() + 1);
+
+	console.log(tomorrow);
+	console.log(date+1);
+	// console.log(currentDate);
 	console.log('server is running on port: ',port);
 });
