663bc678dba553864c79f9d394b65f5a7d1dae79
Divyam Garg <dvm.garg26@gmail.com>
2023-11-16 16:17:37 +0530

    files_updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './App.css';
-import Login from './Pages/Login'
+import {UserLoginPage} from './Pages/Login'
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <UserLoginPage/>
     </div>
   );
 }
