1d5587ad83f04b7d0c49bf295516f14819eedc78
therealsamas <therealsamas@gmail.com>
2023-11-17 02:11:29 +0530

    corscommit

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -125,7 +125,7 @@ export const UserLoginPage = () => {
         fetchData();
     }, []);
     const fetchData = async () => {
-        const data = await fetch("https://restcountries.com/v2/all");
+        const data = await fetch("http://localhost:3000/user/login");
         const json = await data.json();
         console.log(json);
     }
