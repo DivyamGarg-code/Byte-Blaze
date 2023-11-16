9e44c4152985b3187ff749fd6abb153ac5560b7c
Divyam Garg <dvm.garg26@gmail.com>
2023-11-17 04:59:33 +0530

    files_updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { PermissionPopUp } from '../components/PopUp';
-
+import { roomReservationData } from '../utils/roomReservationData';
 
 
 function TableComponent() {
@@ -34,6 +34,7 @@ function TableComponent() {
     const [dates, setDates] = useState([]);
     const [selectedDate, setSelectedDate] = useState('');
     const [tableData, setTableData] = useState({});
+    const [tableData1, setTableData1] = useState({});
     const [timeSlots, setTimeSlots] = useState([]);
     const [popUpState, setPopUpState] = useState(false);
     const togglePopUp = () => {
@@ -46,17 +47,10 @@ function TableComponent() {
         const data = await fetch("http://localhost:3000/getroomdat/roomd");
         const json = await data.json();
         // console.log(json);
-        setTableData(json.resobj);
-       
-        // console.log(Object.keys(json.resobj));
-        // for(let key in json.resobj){
-        //     console.log(json.resobj[key]);
-        //     setTableData(json.resobj[key]);
-        //     break;
-        // }
+        // setTableData(json.resobj); // API Calling
+        setTableData(roomReservationData.resobj);
     }
     useEffect(() => {
-        // Fetch or use the dummyData here
         console.log(tableData);
         console.log(Object.keys(tableData)); // Dates
         setDates(Object.keys(tableData));
@@ -93,7 +87,7 @@ function TableComponent() {
                 <thead>
                     <tr>
                         <th className="border px-4 py-2"></th>
-                        {Object.keys(tableData).map((room) => (
+                        {Object.keys(tableData[selectedDate] || "").map((room) => (
                             <th key={room} className="border px-4 py-2">
                                 {room}
                             </th>
@@ -104,18 +98,17 @@ function TableComponent() {
                     {timeSlots.map((timeSlot) => (
                         <tr key={timeSlot} className="border">
                             <td className="border px-4 py-2">{timeSlot}</td>
-                            {Object.keys(tableData).map((room) => (
+                            {Object.keys(tableData[selectedDate] || "").map((room) => (
                                 <td key={`${room}-${timeSlot}`} className="border px-4 py-2">
-                                    {console.log(tableData[room][timeSlot].state)}
-                                    {/* <button
-                                        className={`${tableData[room][timeSlot].state === 'booked' ? 'bg-red-500 cursor-not-allowed' : tableData[room][timeSlot].state === 'pending' ? 'bg-yellow-500 cursor-not-allowed' : tableData[room][timeSlot].state === 'free' ? 'bg-green-500' : 'bg-yellow-500 cursor-not-allowed'} 
+                                    <button
+                                        className={`${tableData[selectedDate][room][timeSlot].status === 'booked' ? 'bg-red-500 cursor-not-allowed' : tableData[selectedDate][room][timeSlot].status === 'pending' ? 'bg-yellow-500 cursor-not-allowed' : tableData[selectedDate][room][timeSlot].status === 'free' ? 'bg-green-500' : 'bg-yellow-500 cursor-not-allowed'} 
                                          text-white font-bold py-2 px-4 rounded`}
                                         onClick={() => {
-                                            handleSlotClick(room, timeSlot, tableData[room][timeSlot].state);
+                                            handleSlotClick(room, timeSlot, tableData[selectedDate][room][timeSlot].status);
                                         }}
                                     >
-                                        {tableData[room][timeSlot].state}
-                                    </button> */}
+                                        {tableData[selectedDate][room][timeSlot].status}
+                                    </button>
                                 </td>
                             ))}
                         </tr>
---
+++ src/utils/roomReservationData.js
@@ -0,0 +1,420 @@
+export const roomReservationData = {
+    "resobj": {
+        "17-11-2023": {
+            "R1": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "booked",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R2": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "pending",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R3": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            }
+        },
+        "18-11-2023": {
+            "R1": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "booked",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "booked",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "pending",
+                    "occupant": "nil"
+                }
+            },
+            "R2": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R3": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "booked",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            }
+        },
+        "19-11-2023": {
+            "R1": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "pending",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R2": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R3": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            }
+        },
+        "20-11-2023": {
+            "R1": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R2": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            },
+            "R3": {
+                "9-10": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "10-11": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "11-12": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "12-1": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "1-2": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "2-3": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "3-4": {
+                    "status": "free",
+                    "occupant": "nil"
+                },
+                "4-5": {
+                    "status": "free",
+                    "occupant": "nil"
+                }
+            }
+        }
+    }
+}
